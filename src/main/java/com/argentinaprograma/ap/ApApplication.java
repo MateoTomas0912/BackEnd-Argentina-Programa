6b68e02e43d2ae5ce7f262befe1dc169dad676f7
MateoTomas0912 <99299338+MateoTomas0912@users.noreply.github.com>
2022-07-11 12:24:49 -0300

    Update ApApplication.java

--- src/main/java/com/argentinaprograma/ap/ApApplication.java
+++ src/main/java/com/argentinaprograma/ap/ApApplication.java
@@ -35,7 +35,7 @@ public class ApApplication {
 	public CorsFilter corsFilter() {
 		CorsConfiguration corsConfiguration = new CorsConfiguration();
 		corsConfiguration.setAllowCredentials(true);
-		corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
+		corsConfiguration.setAllowedOriginPatterns(Arrays.asList("*"));
 
 		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
 				"Accept", "Authorization", "Origin, Accept", "X-Requested-With",
